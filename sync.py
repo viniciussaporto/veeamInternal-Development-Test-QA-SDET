d56384074e90c99c6c25b605985c54e63b5f1da0
viniciussaporto <vinicius.saporto+github@gmail.com>
2023-06-22 21:27:02 +0100

    copy and error handling with hash implementation

--- sync.py
+++ sync.py
@@ -7,7 +7,6 @@ import shutil
 import time
 
 # Function for initial sync
-
 def	initial_sync(source_folder, replica_folder, log_file):
 	sync_folders(source_folder, replica_folder, log_file)
 
@@ -31,7 +30,51 @@ def	sync_folders(source_folder, replica_folder, log_file):
 			log_messages.append(f'Removed directory: {file_path}')
 	
 	for file in diffs.left_only + diffs.diff_files:
-		
+		src_file = os.path.join(source_folder, file)
+		dst_file = os.path.join(replica_folder, file)
+		if os.path.isfile(src_file):
+			shutil.copy2(src_file, dst_file)
+			log_messages.append(f'Copied file: {src_file} to {dst_file}')
+
+			# Calculate SHA-256
+			src_hash = calculate_sha256_hash(src_file)
+			dst_hash = calculate_sha256_hash(dst_file)
+			if src_hash == dst_hash:
+				log_messages.append(f'Hash match for: {dst_file}')
+			else:
+				log_messages.append(f'Hash mismatch for: {dst_file}')
+				# Handle mismatch and retry
+				attempts = 0
+				while attempts <= 3:
+					shutil.copy2(src_file, dst_file)
+					log_messages.append(f'Overwrote mismatched file: {dst_file}')
+					dst_hash = calculate_sha256_hash(dst_file)
+					if src_hash == dst_hash:
+						log_messages.append(f'Hash matched after {attempts} attemps for file: {dst_file}')
+						break
+					attempts += 1
+				else:
+					log_messages.append(f'Failed to overwrite file after 3 attemps: {dst_file}, try rerunning program')
+		else:
+			shutil.copytree(src_file, dst_file)
+			log_messages.append(f'Copied directory: {src_file} > {dst_file}')
+	# Log every action to console and log file
+	log_to_file(log_file, log_messages)
+	log_to_console(log_messages)
+
+def	calculate_sha256_hash(file_path):
+	with open(file_path, 'rb') as file:
+		hasher = hashlib.sha256()
+		while True:
+			data = file.read(8192)
+			if not data:
+				break
+			hasher.update(data)
+	return hasher.hexdigest()
+
+def	log_to_file():
+
+def	log_to_console():
 
 # Compare copied content
 # Remove files or folders in the replica that are NOT present in the source
