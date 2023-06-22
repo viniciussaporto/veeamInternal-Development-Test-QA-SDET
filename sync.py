0f20a6ba34a976ebb863f3fa22cb1889c9112dc8
viniciussaporto <vinicius.saporto+github@gmail.com>
2023-06-22 22:23:24 +0100

    optimizations and cleaning

--- sync.py
+++ sync.py
@@ -1,6 +1,5 @@
 # Initialitize file with libs
 import argparse
-import filecmp
 import hashlib
 import os
 import shutil
@@ -17,56 +16,53 @@ def	initial_sync(source_folder, replica_folder, log_file):
 		sync_folders(source_folder, replica_folder, log_file)
 
 # Function to sync folders and write actions to log file
-def	sync_folders(source_folder, replica_folder, log_file):
-	diffs = filecmp.dircmp(source_folder, replica_folder) #os.scandir can be more efficient
-	log_messages = []
-    
-	for file in diffs.right_only:
-		file_path = os.path.join(replica_folder, file)
-		# Remove files or folders in the replica that are NOT present in the source
-		if os.path.isfile(file_path):
-			os.remove(file_path)
-			log_messages.append(f'Removed file: {file_path}')
-		else:
-			shutil.rmtree(file_path)
-			log_messages.append(f'Removed directory: {file_path}')
-	
-	for file in diffs.left_only + diffs.diff_files:
-		src_file = os.path.join(source_folder, file)
-		dst_file = os.path.join(replica_folder, file)
-		if os.path.isfile(src_file):
-			shutil.copy2(src_file, dst_file) # It's possible to use shutil.copy for files that don't require metadata preservation
-			log_messages.append(f'Copied file: {src_file} to {dst_file}')
+def sync_folders(source_folder, replica_folder, log_file):
+    log_messages = []
 
-			# It's possible to avoid unnecessary rehashing of every file, and only do it if 'modified date' or 'size' changes
-			# Calculate SHA-256
-			src_hash = calc_sha256(src_file)
-			dst_hash = calc_sha256(dst_file)
-			# Compare copied content
-			if src_hash == dst_hash:
-				log_messages.append(f'Hash match for: {dst_file}')
-			else:
-				# Handle mismatch
-				log_messages.append(f'Hash mismatch for: {dst_file}')
-				attempts = 0
-				# Using SHA-256 it compares the files to search for a corruption,
-				#	if it's wrong, it attempts to overwrite the file with the source.
-				while attempts <= 3:
-					shutil.copy2(src_file, dst_file)
-					log_messages.append(f'Overwrote mismatched file: {dst_file}')
-					dst_hash = calc_sha256(dst_file)
-					if src_hash == dst_hash:
-						log_messages.append(f'Hash matched after {attempts} attemps for file: {dst_file}')
-						break
-					attempts += 1
-				else:
-					log_messages.append(f'Failed to overwrite file after 3 attemps: {dst_file}, try rerunning program')
-		else:
-			shutil.copytree(src_file, dst_file)
-			log_messages.append(f'Copied directory: {src_file} > {dst_file}')
-	# Log actions and print them to console and log file
-	log_to_file(log_file, log_messages)
-	log_to_console(log_messages)
+    # Compare directories using os.scandir() for better performance
+    src_files = {entry.name: entry for entry in os.scandir(source_folder)}
+    dst_files = {entry.name: entry for entry in os.scandir(replica_folder)}
+
+    # Remove files/folders in replica that are not in source
+    for file_name, dst_entry in dst_files.items():
+        dst_path = os.path.join(replica_folder, file_name)
+        if file_name not in src_files:
+            if dst_entry.is_file():
+                os.remove(dst_path)
+                log_messages.append(f'Removed file: {dst_path}')
+            else:
+                shutil.rmtree(dst_path)
+                log_messages.append(f'Removed directory: {dst_path}')
+
+    # Copy new or modified files/folders from source to replica
+    for file_name, src_entry in src_files.items():
+        src_path = os.path.join(source_folder, file_name)
+        dst_path = os.path.join(replica_folder, file_name)
+
+        if file_name not in dst_files:
+            if src_entry.is_file():
+                shutil.copy2(src_path, dst_path)
+                log_messages.append(f'Copied file: {src_path} -> {dst_path}')
+            else:
+                shutil.copytree(src_path, dst_path)
+                log_messages.append(f'Copied directory: {src_path} -> {dst_path}')
+        else:
+            dst_entry = dst_files[file_name]
+
+            if src_entry.is_file() and dst_entry.is_file():
+                # Compare file size and modification time for synchronization
+                if src_entry.stat().st_size != dst_entry.stat().st_size or \
+                   src_entry.stat().st_mtime > dst_entry.stat().st_mtime:
+                    src_hash = calc_sha256(src_path)
+                    dst_hash = calc_sha256(dst_path)
+
+                    if src_hash != dst_hash:
+                        shutil.copy2(src_path, dst_path)
+                        log_messages.append(f'Updated file: {src_path} -> {dst_path}')
+
+    # Log the synchronization operations to both console and log file
+    log_to_file(log_file, log_messages)
+    log_to_console(log_messages)
 
 def	calc_sha256(file_path):
 	with open(file_path, 'rb') as file:
