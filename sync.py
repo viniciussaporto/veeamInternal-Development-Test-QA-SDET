6e90557cd06ca18f2f2e9b54b5cee3d2081611d6
viniciussaporto <vinicius.saporto+github@gmail.com>
2023-06-22 21:05:34 +0100

    intial sync and interval

--- sync.py
+++ sync.py
@@ -1,7 +1,38 @@
 # Initialitize file with libs
+import argparse
+import filecmp
+import hashlib
+import os
+import shutil
+import time
+
 # Function for initial sync
-# Function to continuosly monitor and sync in the set interval
+
+def	initial_sync(source_folder, replica_folder, log_file):
+	sync_folders(source_folder, replica_folder, log_file)
+
+	# Continuosly monitor and sync in the set interval
+	while True:
+		time.sleep(args.interval)
+		sync_folders(source_folder, replica_folder, log_file)
+
 # Function to sync folders and write actions to log file
+def	sync_folders(source_folder, replica_folder, log_file):
+	diffs = filecmp.dircmp(source_folder, replica_folder)
+	log_messages = []
+    
+	for file in diffs.right_only:
+		file_path = os.path.join(replica_folder, file)
+		if: os.path.isfile(file_path)
+			os.remove(file_path)
+			log_messages.append(f'Removed file: {file_path}')
+		else:
+			shutil.rmtree(file_path)
+			log_messages.append(f'Removed directory: {file_path}')
+	
+	for file in diffs.left_only + diffs.diff_files:
+		
+
 # Compare copied content
 # Remove files or folders in the replica that are NOT present in the source
 # Function to copy new and modified files or folders to replica
